773ce8304f745c90e7770783d289022916735fc4
koleksy <k.oleksy@getprintbox.com>
2022-10-27 08:45:35 +0200

    v1

---
+++ app/src/main/java/com/frozen92/rn_aar_lib/MainActivity.java
@@ -0,0 +1,14 @@
+package com.frozen92.rn_aar_lib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
